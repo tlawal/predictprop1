93ed7e6da19fdb8ec68e0f2245aa754257b083ef
tlawal <larry.lawal@gmail.com>
2025-11-05 22:10:29 -0500

    commissions route

--- app/api/commissions/add/route.js
+++ app/api/commissions/add/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { supabaseAdmin, isSupabaseConfigured } from '../../../lib/supabase';
+import { supabaseAdmin, isSupabaseConfigured } from '../../../../lib/supabase';
 
 export async function POST(request) {
   try {
